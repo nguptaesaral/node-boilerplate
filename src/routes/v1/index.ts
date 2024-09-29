60241c61c303c68ddeff005ffd96b69e1f32b640
nikhil <ngupta.esaral@gmail.com>
2024-09-30 03:24:13 +0530

    api authentication middleware

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import path from "path";
 import authRouter from './auth.route'
+import { protect } from "../../middlewares/auth.middleware";
 
 const router = express.Router()
 
@@ -8,5 +9,9 @@ router.get('/status', (req, res) => res.send("ok"))
 router.use('/static', express.static(path.join(__dirname, '../../public')));
 
 router.use('/auth', authRouter)
+
+//authentication required routes.
+router.use(protect)
+router.get("/protected", (_, res) => res.send("Authenticated."))
 
 export default router;
