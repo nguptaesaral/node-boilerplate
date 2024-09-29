c9dc11dde5cbe12c2c910713ffe6279db9215f57
nikhil <ngupta.esaral@gmail.com>
2024-09-30 03:19:16 +0530

    added static root - serving media files

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import path from "path";
 
 const router = express.Router()
 
 router.get('/status', (req, res) => res.send("ok"))
+router.use('/static', express.static(path.join(__dirname, '../../public')));
 
 
 export default router;
