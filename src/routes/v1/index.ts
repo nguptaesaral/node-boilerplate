c88cbab7651f1dac53edf7d71d54cf1b3cbb3b28
nikhil <ngupta.esaral@gmail.com>
2024-09-30 03:22:54 +0530

    added user auth routes

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import path from "path";
+import authRouter from './auth.route'
 
 const router = express.Router()
 
 router.get('/status', (req, res) => res.send("ok"))
 router.use('/static', express.static(path.join(__dirname, '../../public')));
 
+router.use('/auth', authRouter)
 
 export default router;
